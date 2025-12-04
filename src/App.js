abccb8de914a5dc92d724e75e1d031e7919072ff
Raymond <40215421+raymond3572@users.noreply.github.com>
2025-12-04 14:28:17 +0200

    Initial MVP React site scaffold for LineHustle: landing, products, and order functionality.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import ProductsPage from './pages/ProductsPage';
+import OrderPage from './pages/OrderPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/order/:productId" element={<OrderPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
